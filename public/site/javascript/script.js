7b2c035d14020739e6dcd3953c7aa77a937687e0
Aakriti Kumal <aakritikumal14@gmail.com>
2023-05-29 09:59:54 +0545

    electronic-web

---
+++ public/site/javascript/script.js
@@ -0,0 +1,3 @@
+function doalert(){
+    alert("Aakriti");
+}
---
+++ resources/views/site/template.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Electronic | E-commerce</title>
+
+    <link rel="stylesheet" href="{{asset('site/fontawesome/all.css')}}">
+
+    <!-- Bootstrap ko css link gareko -->
+    <link rel="stylesheet" href="{{ asset('site/bootstrap/bootstrap.css') }}">
+
+    <!-- Css link gareko -->
+    <link rel="stylesheet" href="{{asset('site/css/style.css')}}">
+
+</head>
+<body>
+
+
+    <button onclick="doalert()" class="btn btn-danger">Hey</button>
+
+
+
+    <!-- Javascript link gareko -->
+    <script src="{{ asset('site/javascript/script.js')}}"></script>
+
+    <!-- fontawesome link gareko -->
+    <script src="{{asset('site/fontawesome/all.js')}}"></script>
+
+    <!-- Bootstrap ko JS link gareko  -->
+    <script src="{{ asset('site/bootstrap/bootstrap.js')}}"></script>
+
+    <!-- jquery lai link gareko -->
+    <script src="{{asset('site/jquery/jquery.js')}}"></script>
+
+
+
+
+
+</body>
+</html>
